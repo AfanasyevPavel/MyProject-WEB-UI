dd19bef8436a8ab1b04e5aede1825ff25d579811
AfanasyevPavel <afanpav@mail.ru>
2023-06-03 13:25:09 +0300

    lesson3

---
+++ src/main/java/org/example/lesson3/ChangeName.java
@@ -0,0 +1,65 @@
+package org.example.lesson3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.concurrent.TimeUnit;
+public class ChangeName {
+    public static void changeName() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("start-maximized");
+
+        WebDriver webDriver = new ChromeDriver(options);
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        webDriver.get("https://pastilafactory.ru");
+
+        WebElement webElement1 = webDriver.findElement(By.xpath("/html/body/div[1]/div/header/div/div[3]/div[2]/div[3]/button"));
+        webElement1.click();
+
+        Thread.sleep(1000);
+
+        WebElement webElement2 = webDriver.findElement(By.xpath("/html/body/div[1]/div/header/div/div[3]/div[2]/div[3]/ul/li[2]/a"));
+        webElement2.click();
+
+        Thread.sleep(1000);
+
+        WebElement webElement3 = webDriver.findElement(By.xpath("//*[@id=\"login-email\"]"));
+        webElement3. sendKeys("afnpvl@gmail.com");
+
+        Thread.sleep(1000);
+
+        WebElement webElement4 = webDriver.findElement(By.xpath("//*[@id=\"login-password\"]"));
+        webElement4. sendKeys("112233");
+
+        Thread.sleep(1000);
+
+        WebElement webElement5 = webDriver.findElement(By.xpath("//*[@id=\"quick-login\"]/div[4]/button"));
+        webElement5.click();
+
+        Thread.sleep(1000);
+
+        WebElement webElement6 = webDriver.findElement(By.xpath("//*[@id=\"content\"]/div/a[1]"));
+        webElement6.click();
+
+        Thread.sleep(1000);
+
+        WebElement webElement7 = webDriver.findElement(By.xpath("//*[@id=\"input-lastname\"]"));
+        webElement7. clear();
+        webElement7. sendKeys("Сидоров");
+
+        Thread.sleep(1000);
+
+        WebElement webElement8 = webDriver.findElement(By.xpath("//*[@id=\"content\"]/div/form/div/div[2]/input"));
+        webElement8.click();
+
+        Thread.sleep(1000);
+
+        webDriver.quit();
+    }
+}
---
+++ src/main/java/org/example/lesson3/Main.java
@@ -0,0 +1,10 @@
+package org.example.lesson3;
+
+    public class Main {
+        public static void main(String[] args) throws InterruptedException {
+            Login.login();
+            ChangeName.changeName();
+            AddToBasket.addToBasket();
+            SearchQuite.searchQuite();
+        }
+    }
